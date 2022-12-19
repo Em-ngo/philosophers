bd406215f5ae12e4fc92d93c095f29cbda371587
Emma NGO <engo@e3r2p19.clusters.42paris.fr>
2022-12-19 18:49:20 +0100

    init fork debug

--- philo/includes/philo.h
+++ philo/includes/philo.h
@@ -6,7 +6,7 @@
 /*   By: engo <engo@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/14 15:13:33 by engo              #+#    #+#             */
-/*   Updated: 2022/12/17 17:06:07 by engo             ###   ########.fr       */
+/*   Updated: 2022/12/19 17:43:36 by engo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,21 +22,20 @@
 
 typedef struct t_data
 {
-	pthread_mutex_t	time;
+	pthread_t		time;
 	pthread_mutex_t	death;
-	pthread_mutex_t	*l_fork;
-	pthread_mutex_t	*r_fork;
-	int				l_died;
 }				t_data;
 
 typedef struct s_philo
 {
-	pthread_mutex_t	write;
 	int				n_philo;
 	int				t_to_die;
 	int				t_to_eat;
 	int				t_to_sleep;
+	int				last_died;
 	int				meal;
+	pthread_mutex_t	*forks;
+	pthread_mutex_t	write;
 	t_data			*data;
 }				t_philo;
 
@@ -47,9 +46,8 @@ int		check_intmax(int ac, char **av);
 int		check_args(int ac, char **av);
 
 void	init_struct(int ac, char **av, t_philo *philo);
-void	init_forks(t_philo *philo);
+int		init_forks(t_philo *philo);
 
-void	*routine(void *tmp);
 void	*routine(void *tmp);
 void	ft_exec(int ac, char **av, t_philo philo, pthread_t *t1);
 
--- philo/srcs/init_struct.c
+++ philo/srcs/init_struct.c
@@ -6,33 +6,27 @@
 /*   By: engo <engo@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/15 15:58:17 by engo              #+#    #+#             */
-/*   Updated: 2022/12/17 17:24:48 by engo             ###   ########.fr       */
+/*   Updated: 2022/12/19 17:46:24 by engo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-void	init_forks(t_philo *philo)
+
+int	init_forks(t_philo *philo)
 {
-	int	i;
+	int		i;
 
 	i = 0;
 	while (i < philo->n_philo)
 	{
-		philo[i].data->l_fork = malloc(sizeof(pthread_mutex_t));
-		if (!philo[i].data->l_fork)
-			return ;
+		if (pthread_mutex_init(&philo->forks[i], NULL))
+			return (1);
 		i++;
 	}
-	i = 0;
-	while (i < philo->n_philo)
-	{
-		philo[i].data->r_fork = malloc(sizeof(pthread_mutex_t));
-		if (!philo[i].data->r_fork)
-			return ;
-		philo[i].data->r_fork = philo[(i + 1) % philo->n_philo].data->l_fork;
-		i++;
-	}
+	if (pthread_mutex_init(&philo->write, NULL))
+		return (1);
+	return (0);
 }
 
 void	init_struct(int ac, char **av, t_philo *philo)
@@ -42,9 +36,8 @@ void	init_struct(int ac, char **av, t_philo *philo)
 	philo->t_to_die = ft_atoi(av[2]);
 	philo->t_to_eat = ft_atoi(av[3]);
 	philo->t_to_sleep = ft_atoi(av[4]);
-	pthread_mutex_init(&philo->write, NULL);
-	// if (ac == 6)
-		// philo->meal = ft_atoi(av[5]);
-	// else
-		// philo->meal = 0;
+	if (ac == 6)
+		philo->meal = ft_atoi(av[5]);
+	else
+		philo->meal = 0;
 }
